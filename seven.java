457e432e2a1c28fae9e19a8a56090fb6087343e2
Geraldine cardenas <gcardenascas@gmail.com>
2023-11-04 01:41:05 -0500

    cambios en seven

--- seven.java
+++ seven.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 import java.lang.Math;
-public class Main{
+
+public class Main {
     public static void main(String[] args) {
+        // TODO: Crear un objeto Scanner para leer la entrada del usuario
         Scanner scanner = new Scanner(System.in); 
 
+        // TODO: Solicitar al usuario que ingrese el radio de una circunferencia
         System.out.println("Ingrese el radio de una circunferencia");
         double radio = scanner.nextDouble();
-        double longitud = (2 * (Math.PI) * radio);
-        double area = (Math.PI * radio * radio);
-        System.out.println(" La longitud de la circunferencia ");
-        System.out.println(longitud);
-        System.out.println(" El area de la circunferencia ");
-        System.out.println(area);
-
-
-
         
+        // TODO: Calcular la longitud de la circunferencia usando la fórmula 2 * π * radio
+        double longitud = (2 * (Math.PI) * radio);
+
+        // TODO: Calcular el área de la circunferencia usando la fórmula π * radio^2
+        double area = (Math.PI * radio * radio);
+
+        // TODO: Mostrar la longitud de la circunferencia calculada
+        System.out.println("La longitud de la circunferencia");
+        System.out.println(longitud);
+
+        // TODO: Mostrar el área de la circunferencia calculada
+        System.out.println("El área de la circunferencia");
+        System.out.println(area);
     }
 }
